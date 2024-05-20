a8d78ad545a986ee7a6762e1b29184853a405497
SamedovResul <samedovrasul7@gmail.com>
2024-05-20 12:53:22 +0400

    /

--- server.py
+++ server.py
@@ -29,6 +29,7 @@ res = ''
 bin_Data = []
 def Check_image(file):
     img = cv.imread(f'./{file}')
+    os.remove(f'./{file}')
     blank = np.zeros(img.shape[:2], dtype='uint8')
     mask = cv.circle(blank, (img.shape[1]//2,img.shape[0]//2), 100, 255, -1)
     for i, col in enumerate(colors):
